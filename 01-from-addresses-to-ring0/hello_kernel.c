ef8fcb7de20621a6cb8893fab8be1cb48caf423e
jillravaliya <jillahir9999@gmail.com>
2026-01-17 21:04:02 +0530

    day01: rename folder, add hello kernel module and screenshots

---
+++ 01-from-addresses-to-ring0/hello_kernel.c
@@ -0,0 +1,24 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+
+static int __init hello_init(void) {
+	printk(KERN_INFO "Hello from Kernel !, Module loaded !\n");
+	return 0;
+}
+
+
+static void __exit hello_exit(void) {
+	printk(KERN_INFO "Goodbye from Kernel !, Module unloaded !\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("JILL");
+MODULE_DESCRIPTION("My first kernel module !");
+MODULE_VERSION("1.0");
